bbf8b18b3baf08bb3976a08be2ef802c3d9de2d1
Kylie Fauquher <44852559+kfauquher@users.noreply.github.com>
2022-03-31 23:08:45 -0400

    Add files via upload

---
+++ EmailAdmin.java
@@ -0,0 +1,114 @@
+//Author Name: Kylie Fauquher
+//Date: 03/2022
+
+package emailadmin;
+
+import java.util.Scanner;
+
+public class EmailAdmin {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String dept;
+    private int mailbox = 100;
+    private String altEmail;
+    private int defaultPasswordLength = 8;
+    private String email;
+    private String companyEmail = "eyebeem.com";
+    private String phonenumber;
+    private String confirm;
+         
+    //Recieve first and last name.
+    public EmailAdmin(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("New Employee: " + this.firstName + " " + this.lastName);     
+        this.dept = setDept();
+        this.password = randomPassword(defaultPasswordLength);
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + dept.toLowerCase() + "." + companyEmail;
+    }
+    //Ask for department, return department.
+    private String setDept() {
+        System.out.print("Welcome to EyeBeeM " + firstName + " " + lastName + "! Please enter your department number and we will generate the information you need to get started today: \n1 Sales\n2 Development\n3 Accounting\n0 None\n>>> ");
+        Scanner in = new Scanner(System.in);
+        int deptChoice = in.nextInt();
+        if(deptChoice == 1) {
+            return "Sales";
+        }
+        else if(deptChoice == 2) {
+            return "Development";
+        }
+        else if(deptChoice == 3) {
+            return "Accounting";
+        }
+        else
+        {return " ";
+        }
+    }
+
+    // Generate random password.
+    private String randomPassword(int length){
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@$%*";
+        char[] password = new char[length];
+        for(int i=0; i<length; i++){
+            int rand = (int)(Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);        
+        }
+        return new String(password);
+    }
+    //Set mailbox capacity.
+    public void setMailBoxCapacity(int capacity) {
+        this.mailbox = capacity;
+    }
+    //Set alternative email.
+    public void setAltEmail(String altEmail) {
+        this.altEmail = altEmail;
+    }
+    //Change password.
+    public void changePassword(String password) {
+        this.password = password;
+    }
+    public void setPhoneNumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+    
+    public int getBoxCap(){
+        return mailbox;
+    }
+    public String getAltEmail(){
+        return altEmail;
+    }
+     public String getPassword(){
+        return password;
+     }
+     public String getPhoneNumber(){
+        return phonenumber;
+     }
+     public String showInfo() {
+        System.out.print("\n" + "Display Name: " + firstName + " " + lastName + "\nDepartment: " + dept + "\nCompany Email: " + email + "\nTemporary Password: " + password + "\nMailbox Capacity: " + mailbox + "mb" + "\n" + "\nPersonal Email: " + altEmail + "\nPhone number: " + phonenumber);
+        return null;
+     }
+     
+      String setConfirm() {
+         System.out.print("\nDoes this information look correct to you? " + "\n1 Yes" + "\n2 No" + "\n>>> ");
+         Scanner in = new Scanner(System.in);
+        int confirmChoice = in.nextInt();
+        if(confirmChoice == 1) {
+            return "\nWe hope you enjoy your first day, welcome aboard!";
+        }
+        else if(confirmChoice == 2) {
+        return "\nPlease contact HR at 555-555-5555 if anything looks amiss. Enjoy your first day and welcome aboard!";
+        }
+        else
+        {
+        return " ";
+        }
+    }
+}
+
+
+        
+    
+    
+    
+
---
+++ EmailApp.java
@@ -0,0 +1,19 @@
+//Author Name: Kylie Fauquher
+//Date: 03/2022
+
+package emailadmin;
+
+
+public class EmailApp {
+    
+    public static void main(String[]args) {
+        EmailAdmin eAdmin = new EmailAdmin("John", "Smith");
+        eAdmin.setAltEmail("john.smith@hotmail.com");
+        eAdmin.setPhoneNumber("888-888-8888");
+        
+        System.out.println(eAdmin.showInfo());
+        System.out.println(eAdmin.setConfirm());
+        
+    }
+    
+}
